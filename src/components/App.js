e3fb347229cc64b36702f5da5a5f92cd60149804
Alan Patrick <alan.patrick.fv@gmail.com>
2022-06-27 14:11:12 -0300

    Set cart icon

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavHeader from './NavHeader';
 import Home from './Home';
 import ProductList from './ProductList';
+import Cart from './Cart'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path={'/'} element={<Home />} />
           <Route path={'/products'} element={<ProductList />} />
+          <Route path={'/cart'} element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -1,17 +1,29 @@
 import './NavHeader.css';
+import cartLogo from '../images/icons/cart.svg';
 
 export default function NavHeader(props) {
   return (
     <header className="NavHeader">
-      <a className='logo-name' href="/">
+      <a className="logo-name" href="/">
         Online Shop
       </a>
-      <nav className='link-tree'>
-        <a href="/">
+      <nav className="link-tree">
+        <a className="nav-link" href="/">
           <li>Home</li>
         </a>
-        <a href="/products">
+        <a className="nav-link" href="/products">
           <li>Products</li>
+        </a>
+        <a className="cart-logo" href="/cart">
+          <img
+            style={{
+              width: '70%',
+              aspectRation: '1/1',
+              margin: '5px 5px',
+            }}
+            alt="Cart icon"
+            src={cartLogo}
+          />
         </a>
       </nav>
     </header>
